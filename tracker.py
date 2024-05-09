ec4d7908f97d816b494622fa12414d9cd8566c72
sparidae <2992646478@qq.com>
2024-05-09 11:46:11 +0800

    feat: 实现论文中的train

--- tracker.py
+++ tracker.py
@@ -6,13 +6,25 @@ import numpy as np
 
 class Tracker:
     def __init__(self) -> None:
+        # HOGparams
+        self.HOGparams = {
+            "winSize": None,
+            "blockSize": (8, 8),
+            "blockStride": (4, 4),
+            "cellSize": (4, 4),
+            "nbins": 9,
+        }
+
         pass
 
     def init(self, frame, bbox):
         # 使用框定的frame和bbox初始化tracker
         # 初始化hog特征提取器
+        self.hog = cv2.HOGDescriptor(**self.HOGparams)
         # 生成HOG特征图x
+        x = self._gen_feature()
         # 使用二维高斯函数生成训练标签矩阵y
+        y = self._g2()
         # 计算kxx  训练非线性回归器得到alphah
         pass
 
@@ -36,6 +48,26 @@ class Tracker:
         winStride = ()
         a = self.hog.compute(img, winStride=(576, 432))
         return a
+
+    def _train(self, x, y, sigma, lambda_):
+        # 通过原论文实现的训练函数
+        # 返回核化岭回归的解alphaf
+        # TODO 实现DCF
+        k = self._rbf_kernel_correlation(x, x, sigma)
+        alphaf = np.fft.fft2(y) / (np.fft.fft2(k) + lambda_)
+        return alphaf
+
+    def _rbf_kernel_correlation(self, x1, x2, sigma):
+        c = np.fft.ifft2(np.sum(np.conj(np.fft.fft2(x1)) * np.fft.fft2(x2), axis=0))
+        c = np.fft.fftshift(c)  # 将零频率分量设置为频谱中心
+        d = np.sum(x1**2) + np.sum(x2**2) - 2 * c
+        k = np.exp(-1 / (sigma**2) * np.abs(d) / d.size)
+        return k
+
+    def _detect(self, alphaf, x, z, sigma):
+        k = self._rbf_kernel_correlation(z, x, sigma)
+        responses = np.real(np.fft.ifft2(alphaf * np.fft.fft2(k)))
+        return responses
 
 
 if __name__ == "__main__":
